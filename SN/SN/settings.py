8ca360d76581d7708a981bce8676bba85ee2427a
harshalenka <harshalenka51@gmail.com>
2019-03-26 18:44:01 -0500

    application template,static, basic view setup

--- SN/SN/settings.py
+++ SN/SN/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+TEMPLATE_DIR=os.path.join(BASE_DIR,'SocialNetwork/templates/SocialNetwork')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'SN.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
--- SN/SocialNetwork/views.py
+++ SN/SocialNetwork/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
+from django.urls import reverse_lazy
 
+
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,DeleteView,UpdateView)
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
